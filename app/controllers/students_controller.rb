553b2e5d746c85027caa4ced6ce68e51943f74cc
Jeffery <jeffmc825@gmail.com>
2022-02-23 12:55:45 -0600

    commit tree 99a893ced015e0ba160bdf0a2ca216239c45f33b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name] == nil
+                  Student.all
+              else
+                  Student.all.select{ |student| student.last_name.downcase == params[:name] || student.first_name.downcase == params[:name] }
+              end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
